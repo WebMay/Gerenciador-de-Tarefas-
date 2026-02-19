32f29355e35f8f3acc48c0b1bb3d9256f44deb9a
Mayara Renata <mayararenataa@gmail.com>
2026-02-19 02:46:02 -0300

    Estrutura inicial

---
+++ gerenciador.py
@@ -0,0 +1,17 @@
+while True:
+ print("\n Menu do Gerenciador de Lista de tarefas:")
+
+ print("1. Adicionar tarefa")
+ print("2. Ver tarefas")
+ print("3. Atualizar Tarefa")
+ print("2. Completar Tarefa")
+ print("2. Deletar tarefas completadas")
+ print("6. Sair")
+
+ escolha = input("Digite sua escolha: ")
+
+ if escolha == "6":
+  break
+
+
+ print("Programa finalizado")
